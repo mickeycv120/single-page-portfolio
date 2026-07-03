aedcef400885b0a0d889022dfb437827f4b9c4de
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2026-07-03 10:47:31 -0600

    Add contact form focus and submit styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,19 +2,19 @@ export const Form = () => {
   return (
     <form className="flex flex-col gap-4">
       <input
-        className="border-b border-white text-heading-s placeholder:text-white/50 py-3 px-4"
+        className="border-b border-white text-heading-s placeholder:text-white/50 py-3 px-4 focus:outline-0 focus:border-b focus:border-green"
         type="text"
         id="name"
         placeholder="NAME"
       />
       <input
-        className="border-b border-white text-heading-s placeholder:text-white/50 py-3 px-4"
+        className="border-b border-white text-heading-s placeholder:text-white/50 py-3 px-4 focus:outline-0 focus:border-b focus:border-green"
         type="email"
         id="email"
         placeholder="EMAIL"
       />
       <textarea
-        className="border-b border-white text-heading-s placeholder:text-white/50 px-4 py-3 h-26 resize-none"
+        className="border-b border-white text-heading-s placeholder:text-white/50 px-4 py-3 h-26 resize-none focus:outline-0 focus:border-b focus:border-green"
         id="message"
         placeholder="MESSAGE"
       />
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -2,7 +2,7 @@ import { Form } from "../components/Form";
 
 export const Contact = () => {
   return (
-    <section className="bg-dark-gray grid grid-cols-2 gap-48 px-30 py-10 min-w-screen">
+    <section className="bg-dark-gray grid grid-cols-2 gap-48 px-30 py-16 min-w-screen">
       <div className="items-center justify-center">
         <div className="flex flex-col col-span-1 gap-7">
           <h1 className="text-heading-xl font-bold">Contact</h1>
@@ -11,10 +11,18 @@ export const Contact = () => {
             fill in the form, and I'll get back to you as soon as possible.
           </p>
         </div>
+        <div>
+          <img src="src/assets/images/pattern-rings.svg" alt="ring" />
+        </div>
       </div>
 
-      <div className="col-span-1 items-center justify-center">
+      <div className="col-span-1">
         <Form />
+        <div className="flex w-full justify-end">
+          <button className="border-b-2 border-green pb-1 mt-5 text-heading-s hover:text-green">
+            SEND MESSAGE
+          </button>
+        </div>
       </div>
     </section>
   );
