6fac6ea8fa1ba6711ed395aadb78f49ba74d2cd3
Efrain Coutiño Villegas <ecoutino@emt.dev>
2026-07-02 19:11:06 -0600

    Update contact form layout and styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,18 @@
 export const Form = () => {
   return (
-    <form className="grid grid-cols-2 gap-4">
-      <div className="grid grid-cols-2 gap-4">
-        <input className="border-b border-white text-body-l placeholder:text-body-l placeholder:text-gray" type="text" id="name" placeholder="Name" />
-      </div>
+    <form className="flex flex-col gap-4">
+      <input
+        className="border-b border-white text-body-l placeholder:text-[16px] placeholder:text-gray p-3"
+        type="text"
+        id="name"
+        placeholder="NAME"
+      />
+      <input
+        className="border-b border-white text-body-l placeholder:text-[16px] placeholder:text-gray p-3"
+        type="email"
+        id="email"
+        placeholder="EMAIL"
+      />
     </form>
-  )
-}
+  );
+};
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,20 +1,21 @@
-import { Form } from "../components/Form"
+import { Form } from "../components/Form";
 
 export const Contact = () => {
   return (
-    <section className="bg-dark-gray grid grid-cols-2 px-10">
-      <div className="grid grid-cols-2 items-center justify-center">
-        <div>
+    <section className="bg-dark-gray grid grid-cols-2 gap-48 px-30 py-20 min-w-screen">
+      <div className="items-center justify-center">
+        <div className="col-span-1">
           <h1 className="text-heading-xl font-bold">Contact</h1>
           <p className="text-body-l">
-            I would love to hear about your project and how I could help. Please 
+            I would love to hear about your project and how I could help. Please
             fill in the form, and I'll get back to you as soon as possible.
           </p>
         </div>
       </div>
-      <div>
+
+      <div className="col-span-1 items-center justify-center">
         <Form />
       </div>
     </section>
-  )
-}
+  );
+};
