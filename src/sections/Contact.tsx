e66193f348c7ce836f4e3478067dabb5dae16650
Efrain Coutiño Villegas <ecoutino@emt.dev>
2026-07-07 13:53:30 -0600

    Adjust Contact layout and spacing

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -2,18 +2,23 @@ import { Form } from "../components/Form";
 
 export const Contact = () => {
   return (
-    <section className="relative bg-dark-gray grid grid-cols-2 gap-48 px-30 py-16 min-w-screen">
-      <div className="flex flex-col items-center justify-between">
-        <div className="flex flex-col col-span-1 gap-7">
-          <h1 className="text-heading-xl font-bold">Contact</h1>
-          <p className="text-body-l text-gray tracking-normal leading-6.5">
-            I would love to hear about your project and how I could help. Please
-            fill in the form, and I'll get back to you as soon as possible.
-          </p>
+    <section className="relative bg-dark-gray grid grid-cols-2 gap-48 px-30 pt-18 min-w-screen">
+      <div className="flex flex-col overflow-hidden ">
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col gap-8 mb-4">
+            <h1 className="text-heading-xl">Contact</h1>
+            <p className="text-gray">
+              I would love to hear about your project and how I could help.
+              Please fill in the form, and I’ll get back to you as soon as
+              possible.
+            </p>
+          </div>
         </div>
-        <div className="">
-          <img src="src/assets/images/pattern-rings.svg" alt="ring" className="w-[270px] h-[130px] object-none object-right" />
-        </div>
+        <img
+          className="absolute bottom-0 left-0 w-[350px] h-[150px] object-none object-right"
+          alt="rings"
+          src="src/assets/images/pattern-rings.svg"
+        />
       </div>
 
       <div className="col-span-1">
