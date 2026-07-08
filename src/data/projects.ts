2d4426183a3231534d0b5f055ef0da1080839be3
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2026-07-07 22:02:43 -0600

    Add project thumbnail and skills

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,8 +2,8 @@ import type { Project } from "../types/project-type";
 export const data: Project[] = [
   {
     id: 0,
-    image: "",
-    title: "",
-    skills: [],
+    image: "/src/assets/images/thumbnail-project-1-large.webp",
+    title: "DESIGN PORTFOLIO",
+    skills: ["HTML", "CSS"],
   },
 ];
