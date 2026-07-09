e2bb434a8499e94c9d8cf9de4f082f4a5780c1cb
Efrain Coutiño Villegas <ecoutino@emt.dev>
2026-07-08 23:20:26 -0600

    Enhance Card component with overlay buttons for project and code viewing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,13 @@ type CardProps = {
 export const Card = ({ project }: CardProps) => {
   return (
     <article className="flex flex-col gap-2">
-      <img src={project.image} alt={project.title} />
+      <div className="relative inset-0">
+        <div className="flex flex-col gap-4 items-center justify-center absolute w-full h-full bg-black/80 opacity-0 hover:opacity-100">
+          <button>VIEW PROJECT</button>
+          <button>VIEW CODE</button>
+        </div>
+        <img src={project.image} alt={project.title} />
+      </div>
       <h3 className="text-heading-m">{project.title}</h3>
       <ul className="flex flex-row gap-3">
         {project.skills.map((skill) => (
